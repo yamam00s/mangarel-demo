c8255ecfa681bf91ab5140dd40acbe4276c1ee58
yamam00s <s.yamamo0327@gmail.com>
2020-03-07 16:20:12 +0900

    create findOrCreateAuthors

---
+++ functions/src/firestore-admin/author.ts
@@ -0,0 +1,50 @@
+import admin from 'firebase-admin';
+
+import { collectionName } from '../services/mangarel/constants';
+import { BookItem } from '../services/rakuten/models/book-item';
+import { Author, blankAuthor } from '../services/mangarel/models/author';
+import { addCounter } from './record-counter';
+import { normalize, uniform } from '../utils/text-processor';
+
+export const findOrCreateAuthors = async (
+  db: admin.firestore.Firestore,
+  bookItem: BookItem
+) => {
+  const authorsRef = db.collection(collectionName.authors);
+  const authorsName = bookItem.author.split('/');
+  const authorsNameReadings = bookItem.authorKana
+    .split('/')
+    .map(name => name.replace(/\s+/g, ' ').replace(/,/g, ' '));
+
+  const authors: Author[] = authorsName.map((name, i) => ({
+    ...blankAuthor,
+    name: uniform(name),
+    nameReading: uniform(authorsNameReadings[i]),
+    variation: normalize(name)
+  }));
+
+  let i = 0;
+
+  for await (const author of authors) {
+    const snap = await authorsRef
+      .where('variation', '==', normalize(author.name))
+      .get();
+
+    if (snap.size) {
+      authors[i].id = snap.docs[0].id;
+    } else {
+      const docRef = authorsRef.doc();
+      await docRef.set({
+        ...author,
+        createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        updatedAt: admin.firestore.FieldValue.serverTimestamp()
+      });
+      await addCounter(db, collectionName.authors);
+      authors[i].id = docRef.id;
+    }
+
+    i += 1;
+  }
+
+  return authors;
+};
