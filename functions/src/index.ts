ab0ef198cfddcd77cadb1696c690af0cea6e45b8
yamam00s <s.yamamo0327@gmail.com>
2020-03-08 16:15:24 +0900

    register-booksを外部ファイルに&index.tsで実行関数を出し分け

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,73 +1,34 @@
-import * as functions from 'firebase-functions';
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
 import admin from 'firebase-admin';
-import { subDays } from 'date-fns';
+import forEach from 'lodash/forEach';
+import { isDevelopment } from './utils/env';
 
-import { collectionName } from './services/mangarel/constants';
-import { FeedMemo } from './services/mangarel/models/feed-memo';
-import { findBookItem } from './services/rakuten/api';
-import { findOrCreateAuthors } from './firestore-admin/author';
-import { findPublisher } from './firestore-admin/publisher';
-import { createBook } from './firestore-admin/book';
-import { sleep } from './utils/timer';
-
-import rakutenId from './mangarel-demo-rakuten-app-id.json';
-
-const RAKUTEN_APP_ID = rakutenId.id;
-
-// 必ず行う
 admin.initializeApp();
 
-export const registerBooks = functions
-  .region('asia-northeast1') // firestoreで設定したリージョンに合わせる
-  .runWith({
-    timeoutSeconds: 100,
-    memory: '1GB'
-  })
-  .pubsub.schedule('5,10,15 2 1,10,20 * *')
-  .timeZone('Asia/Tokyo')
-  .onRun(async () => {
-    const db = admin.firestore();
-    const yesterday = subDays(new Date(), 1);
-    const snap = await db
-      .collection(collectionName.feedMemos)
-      .where('isbn', '==', null)
-      .where('fetchedAt', '<', yesterday)
-      .limit(200)
-      .get();
+const functionMap = {
+  fetchCalendar: './fetch-calendar',
+  registerBooks: './register-books'
+};
 
-    let count = 0;
+const devFunctionMap = {
+  publishers: './publishers'
+};
 
-    for await (const doc of snap.docs) {
-      const memo = doc.data() as FeedMemo;
-      const title = memo.title || '';
-      const publisherName = memo.publisher || '';
-
-      // rakutenAPIからデータをfetch
-      const bookItem = await findBookItem(
-        { title, publisherName },
-        RAKUTEN_APP_ID
-      );
-
-      if (bookItem) {
-        // bookItemからauthorを登録して返す
-        const authors = await findOrCreateAuthors(db, bookItem);
-        // kodanshaのデータをdbから取得
-        const publisher = await findPublisher(db, 'kodansha');
-        // bookItemからbookを登録して返す
-        const book = await createBook(db, memo, bookItem, authors, publisher);
-        // isbnを更新することで２重登録を防ぐ
-        await doc.ref.update({
-          isbn: book.isbn,
-          fetchedAt: admin.firestore.FieldValue.serverTimestamp()
-        });
-        count += 1;
-      } else {
-        await doc.ref.update({
-          fetchedAt: admin.firestore.FieldValue.serverTimestamp()
-        });
-      }
-      await sleep(1000);
+const loadFunctions = (fnMap: typeof functionMap) => {
+  forEach(fnMap, (path, functionName) => {
+    if (
+      // Cloud Functions が起動されると環境変数process.env.FUNCTION_TARGET にその関数名が入る
+      !process.env.FUNCTION_TARGET ||
+      process.env.FUNCTION_TARGET === functionName
+    ) {
+      module.exports[functionName] = require(path);
     }
-    // eslint-disable-next-line no-console
-    console.log(`Registered ${count} books.`);
   });
+};
+
+const fnMap = isDevelopment()
+  ? { ...functionMap, ...devFunctionMap }
+  : functionMap;
+
+loadFunctions(fnMap);
---
+++ functions/src/register-books.ts
@@ -0,0 +1,66 @@
+/* eslint-disable no-console */
+import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
+import { subDays } from 'date-fns';
+
+import { collectionName } from './services/mangarel/constants';
+import { FeedMemo } from './services/mangarel/models/feed-memo';
+import { findBookItem } from './services/rakuten/api';
+import { findOrCreateAuthors } from './firestore-admin/author';
+import { findPublisher } from './firestore-admin/publisher';
+import { createBook } from './firestore-admin/book';
+import { sleep } from './utils/timer';
+
+module.exports = functions
+  .region(functions.config().locale.region)
+  .runWith({
+    timeoutSeconds: 100,
+    memory: '1GB'
+  })
+  .pubsub.schedule('5,10,15 2 1,10,20 * *')
+  .timeZone(functions.config().locale.timezone)
+  .onRun(async () => {
+    const db = admin.firestore();
+    const yesterday = subDays(new Date(), 1);
+    const snap = await db
+      .collection(collectionName.feedMemos)
+      .where('isbn', '==', null)
+      .where('fetchedAt', '<', yesterday)
+      .limit(200)
+      .get();
+    let count = 0;
+
+    for await (const doc of snap.docs) {
+      const memo = doc.data() as FeedMemo;
+      const title = memo.title || '';
+      const publisherName = memo.publisher || '';
+
+      // rakutenAPIからデータをfetch
+      const bookItem = await findBookItem(
+        { title, publisherName },
+        functions.config().rakuten.app_id
+      );
+
+      if (bookItem) {
+        // bookItemからauthorを登録して返す
+        const authors = await findOrCreateAuthors(db, bookItem);
+        // kodanshaのデータをdbから取得
+        const publisher = await findPublisher(db, 'kodansha');
+        // bookItemからbookを登録して返す
+        const book = await createBook(db, memo, bookItem, authors, publisher);
+        // isbnを更新することで２重登録を防ぐ
+        await doc.ref.update({
+          isbn: book.isbn,
+          fetchedAt: admin.firestore.FieldValue.serverTimestamp()
+        });
+        count += 1;
+      } else {
+        await doc.ref.update({
+          fetchedAt: admin.firestore.FieldValue.serverTimestamp()
+        });
+      }
+      await sleep(1000);
+    }
+    // eslint-disable-next-line no-console
+    console.log(`Registered ${count} books.`);
+  });
