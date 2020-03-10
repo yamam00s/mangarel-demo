cc979b277f239560b04702f92b5390a6dd680b6a
yamam00s <s.yamamo0327@gmail.com>
2020-03-10 23:02:19 +0900

    create src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import firebase from 'firebase/app';
+
 import App from './App';
+import FirebaseApp from './FirebaseApp';
+import { ThemeContext } from './contexts';
+import mangarelTheme from './theme';
+import firebaseConfig from './firebase-config';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'semantic-ui-react';
+import './index.css';
+
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(
+  <BrowserRouter>
+    <FirebaseApp>
+      <ThemeContext.Provider value={mangarelTheme}>
+        <App />
+      </ThemeContext.Provider>
+    </FirebaseApp>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
