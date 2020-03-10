0481806f4e2c4e60c6a83f710cde678d076ab023
yamam00s <s.yamamo0327@gmail.com>
2020-03-10 22:56:21 +0900

    create theme.tsとそのコンテキスト

--- src/contexts.ts
+++ src/contexts.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createContext } from 'react';
+import mangarelTheme from './theme';
 
 type FirebaseContextValue = {
   db: firebase.firestore.Firestore | null;
@@ -8,3 +9,7 @@ type FirebaseContextValue = {
 export const FirebaseContext = createContext<FirebaseContextValue>({
   db: null
 });
+
+export const ThemeContext = createContext(
+  (null as unknown) as typeof mangarelTheme
+);
