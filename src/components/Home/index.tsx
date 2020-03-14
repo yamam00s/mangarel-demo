e462beb246e3b289a7f2dc09a6c344772efcac26
yamam00s <s.yamamo0327@gmail.com>
2020-03-14 18:44:15 +0900

    create Home/index component

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendar from 'containers/ Home/Calendar';
+
+const Home: React.FC = () => (
+  <main>
+    <section>
+      <Calendar />
+    </section>
+  </main>
+);
+
+export default Home;
